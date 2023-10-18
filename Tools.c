395382786696416c37c03bad7331a2615c44d61a
elhachiiimiii <132447236+elhachiiimiii@users.noreply.github.com>
2023-10-18 13:06:53 +0100

    Update Tools.c

--- Tools.c
+++ Tools.c
@@ -1,53 +1,48 @@
 #include "monty.h"
 
 /**
- * open_file - opens a file
- * @file_name: the file namepath
+ * open_file - Opens a file.
+ * @file_path: The file path.
  * Return: void
  */
-
-void open_file(char *file_name)
+void open_file(char *file_path)
 {
-	FILE *fd = fopen(file_name, "r");
+	FILE *file_descriptor = fopen(file_path, "r");
 
-	if (file_name == NULL || fd == NULL)
-		err(2, file_name);
+	if (file_path == NULL || file_descriptor == NULL)
+		err(2, file_path);
 
-	read_file(fd);
-	fclose(fd);
+	read_file(file_descriptor);
+	fclose(file_descriptor);
 }
 
-
 /**
- * read_file - reads a file
- * @fd: pointer to file descriptor
+ * read_file - Reads a file.
+ * @file_descriptor: Pointer to file descriptor.
  * Return: void
  */
-
-void read_file(FILE *fd)
+void read_file(FILE *file_descriptor)
 {
 	int line_number, format = 0;
 	char *buffer = NULL;
-	size_t len = 0;
+	size_t length = 0;
 
-	for (line_number = 1; getline(&buffer, &len, fd) != -1; line_number++)
+	for (line_number = 1; getline(&buffer, &length, file_descriptor) != -1; line_number++)
 	{
 		format = parse_line(buffer, line_number, format);
 	}
 	free(buffer);
 }
 
-
 /**
  * parse_line - Separates each line into tokens to determine
- * which function to call
- * @buffer: line from the file
- * @line_number: line number
- * @format:  storage format. If 0 Nodes will be entered as a stack.
- * if 1 nodes will be entered as a queue.
- * Return: Returns 0 if the opcode is stack. 1 if queue.
+ * which function to call.
+ * @buffer: Line from the file.
+ * @line_number: Line number.
+ * @format: Storage format. If 0, nodes will be entered as a stack.
+ *           If 1, nodes will be entered as a queue.
+ * Return: Returns 0 if the opcode is stack, 1 if queue.
  */
-
 int parse_line(char *buffer, int line_number, int format)
 {
 	char *opcode, *value;
@@ -71,12 +66,12 @@ int parse_line(char *buffer, int line_number, int format)
 }
 
 /**
- * find_func - find the appropriate function for the opcode
- * @opcode: opcode
- * @value: argument of opcode
- * @format:  storage format. If 0 Nodes will be entered as a stack.
- * @ln: line number
- * if 1 nodes will be entered as a queue.
+ * find_func - Finds the appropriate function for the opcode.
+ * @opcode: Opcode.
+ * @value: Argument of opcode.
+ * @format: Storage format. If 0, nodes will be entered as a stack.
+ *          If 1, nodes will be entered as a queue.
+ * @ln: Line number.
  * Return: void
  */
 void find_func(char *opcode, char *value, int ln, int format)
@@ -118,15 +113,14 @@ void find_func(char *opcode, char *value, int ln, int format)
 		err(3, ln, opcode);
 }
 
-
 /**
  * call_fun - Calls the required function.
  * @func: Pointer to the function that is about to be called.
- * @op: string representing the opcode.
- * @val: string representing a numeric value.
- * @ln: line numeber for the instruction.
- * @format: Format specifier. If 0 Nodes will be entered as a stack.
- * if 1 nodes will be entered as a queue.
+ * @op: String representing the opcode.
+ * @val: String representing a numeric value.
+ * @ln: Line number for the instruction.
+ * @format: Format specifier. If 0, nodes will be entered as a stack.
+ *          If 1, nodes will be entered as a queue.
  */
 void call_fun(op_func func, char *op, char *val, int ln, int format)
 {
