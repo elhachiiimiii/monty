db94358be47d52de31554d4a1c5720670937b085
elhachiiimiii <132447236+elhachiiimiii@users.noreply.github.com>
2023-10-18 11:59:03 +0100

    Update Stack_Operations_.c

--- Stack_Operations_.c
+++ Stack_Operations_.c
@@ -1,42 +1,44 @@
 #include "monty.h"
 
 /**
- * multiplyNodes - Multiplies the top two elements of the stack.
- * @stack: Pointer to a pointer pointing to the top node of the stack.
- * @lineNumber: Integer representing the line number of the opcode.
+ * mul_nodes - Adds the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
  */
-void multiplyNodes(stack_t **stack, unsigned int lineNumber)
+void mul_nodes(stack_t **stack, unsigned int line_number)
 {
-    int product;
+	int sum;
 
-    if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-        more_err(8, lineNumber, "mul");
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "mul");
 
-    (*stack) = (*stack)->next;
-    product = (*stack)->n * (*stack)->prev->n;
-    (*stack)->n = product;
-    free((*stack)->prev);
-    (*stack)->prev = NULL;
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n * (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
 }
 
+
 /**
- * moduloNodes - Calculates the modulo of the second top element by the top element of the stack.
- * @stack: Pointer to a pointer pointing to the top node of the stack.
- * @lineNumber: Integer representing the line number of the opcode.
+ * mod_nodes - Adds the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
  */
-void moduloNodes(stack_t **stack, unsigned int lineNumber)
+void mod_nodes(stack_t **stack, unsigned int line_number)
 {
-    int result;
+	int sum;
 
-    if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-        more_err(8, lineNumber, "mod");
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 
-    if ((*stack)->n == 0)
-        more_err(9, lineNumber);
+		more_err(8, line_number, "mod");
 
-    (*stack) = (*stack)->next;
-    result = (*stack)->prev->n % (*stack)->n;
-    (*stack)->n = result;
-    free((*stack)->prev);
-    (*stack)->prev = NULL;
+
+	if ((*stack)->n == 0)
+		more_err(9, line_number);
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n % (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
 }
